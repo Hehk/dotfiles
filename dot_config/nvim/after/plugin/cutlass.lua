17f3bd3e552bffb9ae5c6b52bddb6926361fd228
kyle <ona@lunch.run>
2023-10-13 14:06:48 -0500

    cutlass to fennel

--- dot_config/nvim/after/plugin/cutlass.lua
+++ dot_config/nvim/after/plugin/cutlass.lua
@@ -1,3 +1,4 @@
-vim.keymap.set({ "n", "v" }, "m", "d")
+-- [nfnl] Compiled from after/plugin/cutlass.fnl by https://github.com/Olical/nfnl, do not edit.
+vim.keymap.set({"n", "v"}, "m", "d")
 vim.keymap.set("n", "mm", "dd")
-vim.keymap.set("n", "M", "D")
+return vim.keymap.set("n", "M", "D")
